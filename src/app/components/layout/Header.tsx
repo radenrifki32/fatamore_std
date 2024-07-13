6197601fe365bc22bc6da1922be8acdeb80c8b61
Shotgan <radenmuhamadrifki340@gmail.com>
2024-07-13 15:49:19 +0700

    feat(next-config): change Config Domain Image

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -58,7 +58,8 @@ export default function Header() {
                     className='flex h-10 w-10 items-center justify-center rounded-full p-0 text-sm font-light'
                   >
                     <Image
-                      src={session?.user?.image ?? ''}
+                      unoptimized
+                      src={session?.user?.image || Fuison}
                       alt='image'
                       className='h-full w-full rounded-full object-cover'
                     />
