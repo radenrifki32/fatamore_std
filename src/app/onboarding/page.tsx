dd41aad4ef5bfa557b99963b066368b8aa8dee91
Shotgan <radenmuhamadrifki340@gmail.com>
2024-07-22 19:49:57 +0700

    fix(dashboard): add Work List

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,9 +1,7 @@
 'use client';
-import Image from 'next/image';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next-nprogress-bar';
-import FusionLogo from 'public/images/logo-fusion.png';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 import { trpc } from '@/app/_trpc/client';
 import Button from '@/app/components/buttons/Button';
@@ -11,15 +9,15 @@ export default function OnBoarding() {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const [username, setUsername] = useState<string>('');
   const updateUsername = trpc.user.updateUserById.useMutation();
   const { data, refetch } = trpc.user.getUserById.useQuery(undefined, {
     enabled: false,
   });
   const changeUsername = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const username = new FormData().get('username');
     await updateUsername.mutateAsync({
-      username: username,
+      username: username as string,
     });
     const response = await refetch();
     if (response.status === 'success') {
@@ -35,34 +33,13 @@ export default function OnBoarding() {
   }
   return (
     <div className='flex h-screen w-full flex-col items-center  bg-[#EEEEEE]'>
-      <div className='relative mt-16 flex items-center'>
-        <Image
-          src={data?.data?.attributes?.image_url as string}
-          alt='image'
-          height={150}
-          width={150}
-          className='absolute right-20 rounded-full '
-        />
-        <Image
-          src={FusionLogo.src}
-          height={150}
-          width={150}
-          alt='image-fusion'
-          objectFit='cover'
-        />
-      </div>
+      <div className='relative mt-16 flex items-center'></div>
       {/* <SignedIn>
         <UserButton />
       </SignedIn> */}
       <div className='items-center justify-center'>
         <form onSubmit={(e) => changeUsername(e)}>
-          <input
-            required
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setUsername(e.target.value)
-            }
-            name='username'
-          />
+          <input required name='username' />
           <Button type='submit' variant='primary'>
             Click
           </Button>
